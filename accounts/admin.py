5f47fecc3ae4e2a5943afac2dcd9a70e82d59221
shamshodbekkhatamov@gmail.com <shamshodbekkhatamov@gmail.com>
2025-08-08 15:21:07 +0500

    feat: Create CustomUser model and register in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
+from http.cookiejar import user_domain_match
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model=CustomUser
+    list_display =  ['username', 'email', 'first_name', 'last_name', 'is_staff']
+
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets
+
+admin.site.register(CustomUser,CustomUserAdmin)
