32da561cfa07fb45818fd69637cfbe72e9be3a89
shamshodbekkhatamov@gmail.com <shamshodbekkhatamov@gmail.com>
2025-08-26 23:06:15 +0500

    '3'

--- config/settings.py
+++ config/settings.py
@@ -164,7 +164,7 @@ LOGOUT_REDIRECT_URL = 'home'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
